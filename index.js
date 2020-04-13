aa02bd3856bce4ae75d2b17f9b5a9dc3caa186f9
fadeomar <fadeomar2015@gmail.com>
2020-04-14 02:21:12 +0300

    add port to the app

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log('app is running at http://localhost:5000')
 })
